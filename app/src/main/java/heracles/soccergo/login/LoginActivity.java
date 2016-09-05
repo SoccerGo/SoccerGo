94f3722eaf4d033d994153d88c49d5f463a85f84
Kinst <674705142@qq.com>
2016-09-05 19:42:00 +0800

    登录等待框（未完成）

--- app/src/main/java/heracles/soccergo/login/LoginActivity.java
+++ app/src/main/java/heracles/soccergo/login/LoginActivity.java
@@ -1,9 +1,12 @@
 package heracles.soccergo.login;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import heracles.soccergo.MainActivity;
@@ -35,8 +38,17 @@ public class LoginActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                builder.setView(LayoutInflater.from(LoginActivity.this).inflate(R.layout.dialog_seekbar,null));
+                AlertDialog progBarDialog = builder.create();
+                progBarDialog.show();
+                WindowManager.LayoutParams lp = progBarDialog.getWindow().getAttributes();
+                lp.width=200;
+                lp.height=200;
+                lp.alpha=0;
+                progBarDialog.getWindow().setAttributes(lp);
+//                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                startActivity(intent);
             }
         });
     }
