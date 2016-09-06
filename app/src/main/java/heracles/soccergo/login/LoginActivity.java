c853873d2dc0f3dd88fb5be585ebaf4680a9c267
XYY <1053963770@qq.com>
2016-09-06 19:28:03 +0800

    隐藏了登入的进度条，现在可点按钮直接登入 对ClubFragment添加定位

--- app/src/main/java/heracles/soccergo/login/LoginActivity.java
+++ app/src/main/java/heracles/soccergo/login/LoginActivity.java
@@ -1,11 +1,13 @@
 package heracles.soccergo.login;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import heracles.soccergo.MainActivity;
 import heracles.soccergo.R;
 
 public class LoginActivity extends AppCompatActivity
@@ -35,10 +37,9 @@ public class LoginActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                showProgressDialog();
-
-//                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-//                startActivity(intent);
+//                showProgressDialog();
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -50,7 +51,7 @@ public class LoginActivity extends AppCompatActivity
 
     private void showProgressDialog()
     {
-        progressDialog = new Dialog(LoginActivity.this,R.style.ProgBarTheme_AlphaBackground);
+        progressDialog = new Dialog(LoginActivity.this, R.style.ProgBarTheme_AlphaBackground);
         progressDialog.setContentView(R.layout.dialog_progbar);
         progressDialog.setCancelable(true);
         progressDialog.show();
