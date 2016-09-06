e137b1cf2534bc2f304e0b7f8bc2417f972c5534
Kinst <674705142@qq.com>
2016-09-06 10:07:45 +0800

    加载条（未完成2）

--- app/src/main/java/heracles/soccergo/login/LoginActivity.java
+++ app/src/main/java/heracles/soccergo/login/LoginActivity.java
@@ -1,15 +1,14 @@
 package heracles.soccergo.login;
 
 import android.app.AlertDialog;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
-import heracles.soccergo.MainActivity;
 import heracles.soccergo.R;
 
 public class LoginActivity extends AppCompatActivity
@@ -38,15 +37,14 @@ public class LoginActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                builder.setView(LayoutInflater.from(LoginActivity.this).inflate(R.layout.dialog_seekbar,null));
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this,R.style.ProgBarTheme_AlphaBackground);
+                builder.setView(LayoutInflater.from(LoginActivity.this).inflate(R.layout.dialog_progbar,null));
                 AlertDialog progBarDialog = builder.create();
                 progBarDialog.show();
-                WindowManager.LayoutParams lp = progBarDialog.getWindow().getAttributes();
-                lp.width=200;
-                lp.height=200;
-                lp.alpha=0;
-                progBarDialog.getWindow().setAttributes(lp);
+//                WindowManager.LayoutParams lp = progBarDialog.getWindow().getAttributes();
+//                lp.width=200;
+//                lp.height=200;
+//                progBarDialog.getWindow().setAttributes(lp);
 //                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
 //                startActivity(intent);
             }
