9d5beeade5ca1dcf26bec3fefa44a384bf9bea3b
XYY <1053963770@qq.com>
2016-09-11 12:32:38 +0800

    更新文字颜色

--- app/src/main/java/heracles/soccergo/more/FootballMallActivity.java
+++ app/src/main/java/heracles/soccergo/more/FootballMallActivity.java
@@ -1,15 +1,17 @@
 package heracles.soccergo.more;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import heracles.soccergo.R;
 
-public class FootballMallActivity extends AppCompatActivity {
+public class FootballMallActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        setContentView(R.layout.activity_football_mall);
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_football_mall);
     }
 }
