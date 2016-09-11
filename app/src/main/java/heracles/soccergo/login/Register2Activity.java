606922de07d6526560917cf272e5578fc85a9db3
Kinst <674705142@qq.com>
2016-09-11 15:15:13 +0800

    注册更改2

---
+++ app/src/main/java/heracles/soccergo/login/Register2Activity.java
@@ -0,0 +1,15 @@
+package heracles.soccergo.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import heracles.soccergo.R;
+
+public class Register2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register2);
+    }
+}
